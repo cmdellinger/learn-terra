6390208c2cf485163d27be4d729274acd016f526
Charles M Dellinger <charles.dellinger@gmail.com>
2022-04-24 22:35:26 -0400

    added missing imports for reader functions

--- click-portal/contracts/clicker/src/contract.rs
+++ click-portal/contracts/clicker/src/contract.rs
@@ -1,10 +1,10 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{DepsMut, Env, MessageInfo, Response};
+use cosmwasm_std::{to_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
 use cw2::set_contract_version;
 
 use crate::error::ContractError;
-use crate::msg::{InstantiateMsg};
+use crate::msg::{CountResponse, InstantiateMsg, QueryMsg};
 use crate::state::{State, STATE};
 
 // version info for migration info
