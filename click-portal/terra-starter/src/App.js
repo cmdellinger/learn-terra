6bee4d0b25fb33809b78cc37795b124f484de96c
Charles M Dellinger <charles.dellinger@gmail.com>
2022-04-30 16:11:10 -0400

    added the ability to connect and disconnect a terra wallet and rendered button(s)

--- click-portal/terra-starter/src/App.js
+++ click-portal/terra-starter/src/App.js
@@ -1,6 +1,43 @@
 import './App.css';
+//Bring in the required hooks and possible wallet states
+import { useWallet, WalletStatus } from "@terra-money/wallet-provider";
 
 function App() {
+    // Current wallet status, connect & disconnect functions, available connections
+    const { status, connect, disconnect, availableConnectTypes } = useWallet();
+
+    const renderConnectButton = () => {
+      if (status === WalletStatus.WALLET_NOT_CONNECTED) {
+        return (
+          <div className="connect-wallet-div">
+            <button
+              type="button"
+              key={`connect-EXTENSION`}
+              onClick={() => connect("EXTENSION")}
+              className="cta-button connect-wallet-button"
+            >
+              Connect wallet
+            </button>
+          </div>
+        );
+      }
+      // Check if wallet is connect
+      else if (status === WalletStatus.WALLET_CONNECTED) {
+        return (
+          <button
+            type="button"
+            onClick={() => disconnect()}
+            className="cta-button connect-wallet-button"
+          >
+            Disconnect
+          </button>
+        );
+      }
+    };
+    
+    // Let's take a look at what the starting states are!
+    console.log("Wallet status is ", status);
+    console.log("Available connection types:", availableConnectTypes);
 
   return (
     <main className="App">
@@ -18,6 +55,8 @@ function App() {
           alt="Goblin gif"
         />
       </div>
+
+      {renderConnectButton()}
     </main>
   );
 }
